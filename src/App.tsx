a33e8ed501bf12c925c773e2a74298b3c42ec4a1
wangxiaoyun <wangyun@bonc.com.cn>
2026-05-17 19:15:09 +0800

    feat: 双列布局，左列计时器，右列任务+周图

--- src/App.tsx
+++ src/App.tsx
@@ -18,20 +18,24 @@ export default function App() {
 
   return (
     <div className="min-h-[100dvh] bg-neutral-50 text-neutral-900 dark:bg-neutral-950 dark:text-white">
-      <div className="mx-auto flex min-h-[100dvh] max-w-2xl flex-col px-4 sm:px-6">
+      <div className="mx-auto flex min-h-[100dvh] max-w-5xl flex-col px-6">
         <Header />
-        <main className="flex flex-1 flex-col items-center gap-8 pb-12 pt-4 sm:pt-8">
-          {/* Timer section */}
-          <TimerDisplay />
-
-          {/* Divider */}
-          <div className="w-full border-t border-neutral-100 dark:border-neutral-800" />
-
-          {/* Stats and Tasks */}
-          <div className="flex w-full flex-col gap-8">
+        <main className="flex flex-1 gap-8 pb-8 pt-4">
+          {/* 左列：计时器 + 统计 */}
+          <div className="flex w-[380px] shrink-0 flex-col gap-8">
+            <TimerDisplay />
+            <div className="border-t border-neutral-100 dark:border-neutral-800" />
             <StatsPanel />
-            <WeekChart />
+          </div>
+
+          {/* 列分隔线 */}
+          <div className="w-px bg-neutral-100 dark:bg-neutral-800" />
+
+          {/* 右列：任务列表 + 周图表 */}
+          <div className="flex min-w-0 flex-1 flex-col gap-8">
             <TaskList />
+            <div className="border-t border-neutral-100 dark:border-neutral-800" />
+            <WeekChart />
           </div>
         </main>
       </div>
